90e00b628ff39ec9a33895d8de860f2c4f12c001
Tharun <tharuntamilmani200601@gmail.com>
2026-08-22 11:28:06 +0530

    Create ctree.java

---
+++ trees/ctree.java
@@ -0,0 +1,26 @@
+class node{
+    int data;
+    node left;
+    node right;
+    public node(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+}
+public class binarytree {
+    public static void main(String[] args) {
+        node root = new node(3);
+        root.left = new node(2);
+        root.left.left=new node(4);
+        root.left.right=new node(8);
+        root.left.right.left=new node(5);
+        root.left.right.right=new node(6);
+
+        root.right = new node(1);
+        root.right.left=new node(9);
+        root.right.right=new node(10);
+        root.right.right.right=new node(0);
+        System.out.println("Tree successfully constructed with no errors!");
+    }
+}
