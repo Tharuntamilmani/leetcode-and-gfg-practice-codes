90cef203a3a3a3eaded6b18108db3dc3c5bd6b8d
Tharun <tharuntamilmani200601@gmail.com>
2026-08-27 13:33:02 +0530

    Add deleteNode method to BST implementation
    Implement deleteNode method for binary search tree.

---
+++ trees/BST/deleteNode.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int replace(TreeNode root){
+        while(root.right!=null){
+            root=root.right;
+        }
+        return root.val;
+    }
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null){return null;}
+        if(root.val<key){
+            root.right=deleteNode(root.right,key);
+        }
+        else if(root.val>key){
+            root.left=deleteNode(root.left,key);
+        }
+        else{
+            if(root.left==null && root.right==null){
+                return null;
+            }
+            if(root.left==null){
+                return root.right;
+            }
+            if(root.right==null){
+                return root.left;
+            }
+            int temp = replace(root.left);
+            root.val=temp;
+
+            root.left=deleteNode(root.left,temp);
+        }
+        return root;
+    }
+}
